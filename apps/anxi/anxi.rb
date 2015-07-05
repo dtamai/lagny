95a2e33fda9fd6149c8b75c94e9e85bad86ebff8
Daniel Tamai <daniel.tamai@gmail.com>
2015-07-05 00:42:18 -0300

    Create very simple consumer to recreate the CSV

---
+++ apps/anxi/anxi.rb
@@ -0,0 +1,8 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+require "poseidon"
+require "avro"
+
+require "kerala/kerala"
+
+require "lib/topic_consumer"
---
+++ apps/anxi/lib/topic_consumer.rb
@@ -0,0 +1,36 @@
+module Anxi
+  class TopicConsumer
+
+    def initialize(conn_string, topic)
+      host, port = conn_string.split(":")
+      @consumer = Poseidon::PartitionConsumer.new(
+        "anxi", host, port, topic, 0, 0)
+    end
+
+    def consume
+      messages = consumer.fetch
+      messages.each do |m|
+        format, schema_id, payload = m.value.unpack("LLA*")
+        next unless format == Kerala::Serializer::FORMAT
+
+        yield decode(schema_id, payload)
+      end
+      nil
+    end
+
+    private
+
+    attr_reader :consumer
+
+    def schema_for(schema_id)
+      Kerala::SchemaRegister.fetch(schema_id).value
+    end
+
+    def decode(schema_id, payload)
+      reader = Avro::IO::DatumReader.new schema_for(schema_id)
+      decoder = Avro::IO::BinaryDecoder.new(StringIO.new(payload))
+      reader.read(decoder)
+    end
+
+  end
+end
