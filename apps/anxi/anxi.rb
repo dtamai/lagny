a75b125ed23f399857ad0ea504f2c81f4e7494d5
Daniel Tamai <daniel.tamai@gmail.com>
2015-07-09 14:55:18 -0300

    Add script to import original CSV into Kerala

--- apps/anxi/anxi.rb
+++ apps/anxi/anxi.rb
@@ -8,3 +8,4 @@ require "kerala/kerala"
 
 require "lib/topic_consumer"
 require "lib/csv_writer"
+require "lib/csv_to_kerala_migrator"
---
+++ apps/anxi/lib/csv_to_kerala_migrator.rb
@@ -0,0 +1,38 @@
+module Anxi
+  class CSVToKeralaMigrator
+
+    SUPPORTED_SCHEMA = 1
+
+    def initialize(file, producer)
+      @file = file
+      @producer = producer
+    end
+
+    def migrate
+      csv.each do |line|
+        spending = Kerala::Spending.new(line)
+        @producer.send_message("spending", message_for(spending))
+      end
+    end
+
+    private
+
+    def csv
+      options = {
+        :headers => true,
+        :header_converters => :symbol,
+        :return_headers => true
+      }
+      CSV.new(@file, options).tap(&:shift)
+    end
+
+    def schema
+      @schema ||= Kerala::SchemaRegister.fetch(SUPPORTED_SCHEMA)
+    end
+
+    def message_for(object)
+      Kerala::Serializer.new(object, schema).serialize
+    end
+
+  end
+end
