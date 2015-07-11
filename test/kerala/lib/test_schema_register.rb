2c001c39909fc3f9675efe8ec8c7e3e5d2f769df
Daniel Tamai <daniel.tamai@gmail.com>
2015-07-11 00:14:10 -0300

    Refactor test

--- test/kerala/lib/test_schema_register.rb
+++ test/kerala/lib/test_schema_register.rb
@@ -4,11 +4,40 @@ require "kerala"
 module Kerala
   class TestSchemaRegister < Minitest::Test
 
+    def setup
+      Kerala::Config.schemas = {}
+    end
+
+    def test_returns_unknown_schema_when_not_registered
+      schema = SchemaRegister.fetch(2)
+
+      assert_instance_of UnknownSchema, schema
+    end
+
     class TestKnownSchema < Minitest::Test
+
       def setup
         Kerala::Config.schemas = {
-          schema_id => filename
+          1 => "fake.avsc"
         }
+      end
+
+      def test_retrieves_schema
+        schema = fetch_fake_schema(1)
+
+        assert_instance_of Schema, schema
+      end
+
+      def test_assigns_id
+        schema = fetch_fake_schema(1)
+
+        assert_equal schema.id, 1
+      end
+
+      def test_assigns_value
+        schema = fetch_fake_schema(1)
+
+        refute_nil schema.value
       end
 
       def fetch_fake_schema(id)
@@ -18,46 +47,9 @@ module Kerala
         @schema
       end
 
-      def test_retrieves_schema
-        schema = fetch_fake_schema(schema_id)
-
-        assert_instance_of Schema, schema
-      end
-
-      def test_assigns_id
-        schema = fetch_fake_schema(schema_id)
-
-        assert_equal schema.id, schema_id
-      end
-
-      def test_assigns_value
-        schema = fetch_fake_schema(schema_id)
-
-        refute_nil schema.value
-      end
-
-      def schema_id
-        1
-      end
-
-      def filename
-        "fake.avsc"
-      end
-
       def fake_schema
         %q|{ "name": "fake", "type": "string" }|
       end
     end
-
-    def test_returns_unknown_schema_when_not_registered
-      schema = SchemaRegister.fetch(unknown_schema)
-
-      assert_instance_of UnknownSchema, schema
-    end
-
-    def unknown_schema
-      2
-    end
-
   end
 end
