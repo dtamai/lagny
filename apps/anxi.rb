de999fd66c56b24dd039e5f00f0b4acbb57ab3d5
Daniel Tamai <daniel.tamai@gmail.com>
2015-08-09 22:04:43 -0300

    Create spendings table
    As of now the table will be recreated every time Anxi is required.

--- apps/anxi.rb
+++ apps/anxi.rb
@@ -11,3 +11,14 @@ require "anxi/lib/csv_to_kerala_migrator"
 
 Anxi::DB = Sequel.connect("sqlite://tmp/anxi.db")
 
+Anxi::DB.create_table!(:spendings) do
+  primary_key :id
+  String :date, :fixed => true, :size => 10, :null => false
+  String :currency, :fixed => true, :size => 3, :null => false
+  Integer :cents, :null => false
+  String :pay_method, :null => false
+  String :seller, :null => false
+  String :category, :null => false
+  String :tags
+  String :description, :null => false
+end
