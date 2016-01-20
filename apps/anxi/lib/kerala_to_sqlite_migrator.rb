ee76f4295b79e7f0ec323f273a1bf5583fb81fdb
Daniel Tamai <daniel.tamai@gmail.com>
2016-01-20 21:41:39 -0200

    Quick and dirty chargeback processing

--- apps/anxi/lib/kerala_to_sqlite_migrator.rb
+++ apps/anxi/lib/kerala_to_sqlite_migrator.rb
@@ -9,9 +9,30 @@ module Anxi
     end
 
     def migrate
-      @consumer.consume do |msg|
-        @writer.write(Kerala::AddSpending.new(msg))
+      @consumer.consume do |event|
+        case event
+        when Kerala::AddSpending then @writer.write event
+        when Kerala::AddChargeback then process_chargeback event
+        else next
+        end
       end
+    end
+
+    def process_chargeback(event)
+      candidates = Anxi::DB[:spendings].where(
+        date:       event.date,
+        currency:   event.currency,
+        cents:      event.cents,
+        pay_method: event.pay_method,
+        seller:     event.seller).order(:date).limit(1)
+
+      Kerala::Logger.warn "Unmatched chargeback: #{event.attributes}" if candidates.empty?
+
+      candidates.delete
+    end
+
+    def process_spending(event)
+      @writer.write event
     end
   end
 end
