2908e0950b267ba7239ca020e688797123e717f9
Daniel Tamai <daniel.tamai@gmail.com>
2016-03-03 22:02:46 -0300

    Fix metadata not being saved when key is missing

--- apps/anxi/lib/metadata/sql.rb
+++ apps/anxi/lib/metadata/sql.rb
@@ -10,7 +10,11 @@ module Anxi
       end
 
       def set(key, value)
-        @table.where(:key => key.to_s).update(:value => value)
+        @table.where(:key => key.to_s).tap do |row|
+          if row.update(:value => value) == 0
+            row.insert(:key => key.to_s, :value => value)
+          end
+        end
       end
     end
   end
