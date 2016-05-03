cb1f55a9aa8dcc262a1e275945bcf099c2c016e7
Daniel Tamai <daniel.tamai@gmail.com>
2016-05-02 22:17:31 -0300

    Remove schema_id from entity classes
    Not required anymore

--- apps/kerala/entities/snapshot/add_or_update_bucket.rb
+++ apps/kerala/entities/snapshot/add_or_update_bucket.rb
@@ -1,10 +1,6 @@
 module Kerala
   module Snapshot
     class AddOrUpdateBucket < Event
-      def schema_id
-        10
-      end
-
       attribute :bucket,       String, :default => "unknown".freeze
       attribute :display_name, String, :default => "unknown".freeze
       attribute :category,     String, :default => "unknown".freeze
--- apps/kerala/entities/snapshot/add_or_update_category.rb
+++ apps/kerala/entities/snapshot/add_or_update_category.rb
@@ -1,10 +1,6 @@
 module Kerala
   module Snapshot
     class AddOrUpdateCategory < Event
-      def schema_id
-        9
-      end
-
       attribute :category,     String, :default => "unknown".freeze
       attribute :display_name, String, :default => "unknown".freeze
     end
